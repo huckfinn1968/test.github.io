cac256dd7cbf5ce0ed0ad811c9e93dc56304f2fc
huckfinn1968 <42816171+huckfinn1968@users.noreply.github.com>
2024-03-28 19:45:06 -0600

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,28 @@
+sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt-get install seclists -y
+sudo apt-get install dirbuster -y 
+sudo apt-get install gobuster -y
+sudo apt-get install dirsearch -y
+sudo apt-get install neo4j -y
+sudo apt-get install bloodhound -y
+sudo apt-get install git
+mkdir Tools
+mkdir Environments
+mkdir Platforms
+cd Platforms
+mkdir HTB
+mkdir THM
+mkdir echoCTF
+mkdir Offensive
+cd ..
+mkdir Courser
+mkdir MASA
+cd MASA
+git clone https://github.com/MobSF/Mobile-Security-Framework-MobSF.git
+git clone https://github.com/corpnewt/ProperTree.git
+sudo apt install docker-compose -y
+cd ..
+sudo apt-get install apktool -y
+sudo apt install jd-gui -y
+sudo apt install jadx -y
+
