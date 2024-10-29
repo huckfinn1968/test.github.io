a003345968fff04f2eed66c922c39940b518796f
huckfinn1968 <42816171+huckfinn1968@users.noreply.github.com>
2024-10-28 23:55:18 -0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -31,8 +31,8 @@ git clone https://github.com/corpnewt/ProperTree.git
 sudo apt install docker-compose -y
 cd ..
 sudo apt-get install apktool -y
-sudo apt install jd-gui -y
-sudo apt install jadx -y
+sudo apt-get install jd-gui -y
+sudo apt-get install jadx -y
 pip install urllib3==1.25.11
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
